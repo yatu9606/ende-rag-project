0125b8b30cbc041b43d67f1d8a300e64a5a02a89
yatu9606 <yatu22ise@cmrit.ac.in>
2026-02-03 00:20:28 +0530

    fix: removed unused comments

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from query import semantic_search
 
 print("Starting Endee Semantic Search Demo")
 
-# 🔥 ONE shared VectorStore
+
 vector_store = VectorStore(
     collection_name="rag_demo",
     embedding_dim=384
 )
 
-# Ingest data
+
 ingest(vector_store)
 
-# Query loop
+
 while True:
     q = input("\nEnter query (or type 'exit'): ")
     if q.lower() == "exit":
